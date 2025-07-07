8cca973b225a0abb49a23f12adbbcabf7d5e2acc
Nimesh Srivastava <snimesh412@gmail.com>
2025-07-08 02:31:43 +0530

    init matching engine, code restructure

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,26 @@
-use std::collections::HashMap;
-
-#[derive(Debug)]
-enum BidOrAsk {
-    Bid,
-    Ask
-}
-
-#[derive(Debug)]
-struct Orderbook {
-    asks: HashMap<Price, Limit>,
-    bids: HashMap<Price, Limit>,
-}
-
-impl Orderbook {
-    fn new() -> Orderbook {
-        Orderbook {
-            asks: HashMap::new(),
-            bids: HashMap::new(),
-        }
-    }
-
-    fn add_order(&mut self, price: f64, order: Order) {
-        match order.bid_or_ask {
-            BidOrAsk::Bid => {
-                let price = Price::new(price);
-
-                match self.bids.get_mut(&price) {
-                    Some(limit) => {
-                        limit.add_order(order);
-                    }
-                    None => {
-                        let mut limit = Limit::new(price);
-                        limit.add_order(order);
-                        self.bids.insert(price, limit);
-                    }
-                }
-            }
-            BidOrAsk::Ask => {}
-        }
-    }
-}
-
-#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
-struct Price {
-    integral: u64,
-    fractional: u64,
-    scalar: u64,
-}
-
-impl Price {
-    fn new(price: f64) -> Price {
-        let scalar = 100000;
-        let integral = price as u64;
-        let fractional = ((price % 1.0) * scalar as f64) as u64;
-        Price {
-            scalar,
-            integral,
-            fractional,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Limit {
-    price: Price,
-    orders: Vec<Order>,
-}
-
-impl Limit {
-    fn new(price: Price) -> Limit {
-        Limit {
-            price,
-            orders: Vec::new(),
-        }
-    }
-
-    fn add_order(&mut self, order: Order) {
-        self.orders.push(order);
-    }
-}
-
-#[derive(Debug)]
-struct Order {
-    size: f64,
-    bid_or_ask: BidOrAsk,
-}
-
-impl Order {
-    fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
-        Order {bid_or_ask, size}
-    }
-}
+mod matching_engine;
+use matching_engine::orderbook::{Order, BidOrAsk, Orderbook};
+use matching_engine::engine::{MatchingEngine, TradingPair};
 
 fn main() {
-    let buy_order_alpha = Order::new(BidOrAsk::Bid, 5.5);
-    let buy_order_beta = Order::new(BidOrAsk::Bid, 6.5);
-
-    let sell_order = Order::new(BidOrAsk::Ask, 2.5);
-
+    let buy_order_alpha = Order::new(BidOrAsk::Bid, 1.5);
+    let buy_order_beta = Order::new(BidOrAsk::Bid, 2.5);
     let mut orderbook = Orderbook::new();
     orderbook.add_order(4.4, buy_order_alpha);
     orderbook.add_order(4.4, buy_order_beta);
 
-    println!("{:?}", orderbook);
+    let sell_order_alpha = Order::new(BidOrAsk::Ask, 3.5);
+    let sell_order_beta = Order::new(BidOrAsk::Ask, 4.5);
+    orderbook.add_order(10.0, sell_order_alpha);
+    orderbook.add_order(10.0, sell_order_beta);
+
+    //println!("{:?}", orderbook);
+
+    let mut engine = MatchingEngine::new();
+    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
+
+    engine.add_new_market(pair.clone());
+    
+    let buy_order = Order::new(BidOrAsk::Bid, 6.5);
+    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
 }
